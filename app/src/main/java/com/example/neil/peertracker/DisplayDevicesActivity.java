dd9f205be67de07ec19262a434418da471fc4892
Neil Calabroso <nmcalabroso@up.edu.ph>
2014-11-30 10:44:33 +0800

    Adds main activity screen

--- app/src/main/java/com/example/neil/peertracker/DisplayDevicesActivity.java
+++ app/src/main/java/com/example/neil/peertracker/DisplayDevicesActivity.java
@@ -1,26 +1,24 @@
 package com.example.neil.peertracker;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 
-public class MainActivity extends Activity {
-    
+public class DisplayDevicesActivity extends Activity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_display_devices);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.menu_display_devices, menu);
         return true;
     }
 
@@ -30,6 +28,12 @@ public class MainActivity extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        return id == R.id.action_settings || super.onOptionsItemSelected(item);
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
